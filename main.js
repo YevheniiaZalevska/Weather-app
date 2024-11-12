1c76faa28ef9b587d707cf58ed327b356251ba7f
YevheniiaZalevska <yevhenia05@gmail.com>
2024-11-12 12:57:39 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,50 +1,29 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const interBubble = document.querySelector('.interactive');
-    let curX = 0;
-    let curY = 0;
-    let tgX = 0;
-    let tgY = 0;
 
-    const move = () => {
-        curX += (tgX - curX) / 20;
-        curY += (tgY - curY) / 20;
-        interBubble.style.transform = `translate(${Math.round(curX)}px, ${Math.round(curY)}px)`;
-        requestAnimationFrame(move);
-    };
 
-    window.addEventListener('mousemove', (event) => {
-        tgX = event.clientX;
-        tgY = event.clientY;
-    });
-
-    move();
-});
-
-const button = document.querySelector('.butt');
-const errorMsg = document.querySelector('.error');
-const temp = document.querySelector('.temp');
-const cityName = document.querySelector('.city-name');
-const weatherImg = document.querySelector('.weather-img');
-const description = document.querySelector('.description');
-const feelsLike = document.querySelector('.feels-like');
-const pressure = document.querySelector('.pressure');
-const humidity = document.querySelector('.humidity');
-const windSpeed = document.querySelector('.wind-speed');
-const clouds = document.querySelector('.clouds');
-const visibility = document.querySelector('.visibility');
-const pollutionImg = document.querySelector('.pollution-img');
-const pm25 = document.querySelector('.pollution-value');
-const input = document.querySelector('.inp');
+const input = document.querySelector('input');
+const button = document.querySelector('button');
+const errorMsg = document.querySelector('p.error');
+const cityName = document.querySelector('h2.city-name');
+const weatherImg = document.querySelector('img.weather-img');
+const temp = document.querySelector('p.temp');
+const description = document.querySelector('p.description');
+const feelsLike = document.querySelector('span.feels-like');
+const pressure = document.querySelector('span.pressure');
+const humidity = document.querySelector('span.humidity');
+const windSpeed = document.querySelector('span.wind-speed');
+const clouds = document.querySelector('span.clouds');
+const visibility = document.querySelector('span.visibility');
+const pollutionImg = document.querySelector('img.pollution-img');
+const pm25 = document.querySelector('p.pollution-value');
 
 const apiLink = 'https://api.openweathermap.org/data/2.5/weather?q=';
 const apiKey = '&appid=2dbc7632fbfe2a2c4dc8a85c71f953a8';
 const apiUnits = '&units=metric';
 const apiLang = '&lang=pl';
 
-function checkWeatherInfo() {
+function getWeatherInfo() {
     const apiCity = input.value;
     const apiURL = `${apiLink}${apiCity}${apiKey}${apiUnits}${apiLang}`;
-    // console.log(apiURL);
 
     axios.get(apiURL).then((response) => {
         console.log(response.data);
@@ -53,17 +32,20 @@ function checkWeatherInfo() {
         weatherImg.src = `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`;
         temp.textContent = `${Math.floor(response.data.main.temp)}°C`;
         description.textContent = `${response.data.weather[0].description}`;
-        description.classList.add('description-color');
+        description.classList.add('description_color');
         feelsLike.textContent = `${Math.floor(response.data.main.feels_like)}°C`;
         pressure.textContent = `${response.data.main.pressure} hPa`;
         humidity.textContent = `${response.data.main.humidity} %`;
         windSpeed.textContent = `${Math.floor(response.data.wind.speed * 3.6)} km/h`;
         clouds.textContent = `${response.data.clouds.all} %`;
         visibility.textContent = `${response.data.visibility / 1000} km`;
+        errorMsg.textContent = '';
 
-        const airPollutionApi = `https://api.openweathermap.org/data/2.5/air_pollution?lat=${response.data.coord.lat}&lon=${response.data.coord.lon}${apiKey}`;
+        //air pollution api
+        const airPollutionApi = `http://api.openweathermap.org/data/2.5/air_pollution?lat=${response.data.coord.lat}&lon=${response.data.coord.lon}${apiKey}`;
+
         axios.get(airPollutionApi).then((res) => {
-            console.log(res.data.list);
+            console.log(res.data.list)
             pm25.textContent = `${res.data.list[0].components.pm2_5}`;
 
             const pm25Value = res.data.list[0].components.pm2_5;
@@ -80,17 +62,17 @@ function checkWeatherInfo() {
             } else {
                 pollutionImg.style.backgroundColor = 'red';
             }
-    })
+        })
 
-    }).catch ((error) => {
-        console.log(error.response);
+    }).catch((error) => {
+        // console.log(error.response);
 
         errorMsg.textContent = `${error.response.data.message}`;
         [cityName, temp, description, feelsLike, pressure, humidity, windSpeed, clouds, visibility, pm25].forEach((element) => {
             element.textContent = '';
         })
         weatherImg.src = '';
-        description.classList.remove('description-color');
+        description.classList.remove('description_color');
         pollutionImg.style.backgroundColor = 'transparent';
 
     }).finally(() => {
@@ -98,11 +80,11 @@ function checkWeatherInfo() {
     })
 }
 
-const checkWeatherInfoByEnter = (e) => {
+const getWeatherInfoByEnter = (e) => {
     if (e.key === 'Enter') {
-        checkWeatherInfo();
+        getWeatherInfo();
     }
 }
 
-button.addEventListener('click', checkWeatherInfo);
-input.addEventListener('keydown', checkWeatherInfoByEnter);
+input.addEventListener('keydown', getWeatherInfoByEnter);
+button.addEventListener('click', getWeatherInfo);
